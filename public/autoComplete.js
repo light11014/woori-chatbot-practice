5b8710719f97955c0432f9e538f0457b0375f5a8
seonjiwon <tommys915@gmail.com>
2026-01-07 15:42:13 +0900

    feature: 자동완성 화면 출력

--- public/autoComplete.js
+++ public/autoComplete.js
@@ -1,11 +1,10 @@
-const inputTextarea2 = document.getElementById("inputTextarea");
-const [autoComplete1, autoComplete2, autoComplete3] =
-    document.querySelector(".auto-complete");
+const inputTextarea2 = document.getElementById("input-textarea");
+const autoCompleteArea = document.querySelector(".auto-complete-container");
 
 let timerId;
 inputTextarea2.addEventListener("input", (event) => {
     clearTimeout(timerId);
-
+    console.log(1);
     timerId = setTimeout(() => {
         // 입력값 가져오기
         const text = event.target.value;
@@ -15,15 +14,26 @@ inputTextarea2.addEventListener("input", (event) => {
         );
         // 2. localStorage에서 키워드 가져오기
         const keywords = window.localStorage.getItem("keywords");
-        const parsedKeywords = JSON.parse(keywordsRaw);
+        const parsedKeywords = JSON.parse(keywords);
 
         const filteredList = parsedKeywords.filter((value) =>
             value.startsWith(text)
         );
 
+        console.log(filteredList);
         // 3. 렌더링
-        autoComplete1.innerHTML = filteredList[0];
-        autoComplete2.innerHTML = filteredList[1];
-        autoComplete3.innerHTML = filteredList[2];
+        renderKeywords(filteredList);
     }, 1000);
 });
+
+// 화면에 리스트를 그려주는 함수
+function renderKeywords(list) {
+    // 기존 리스트 초기화
+    autoCompleteArea.innerHTML = "";
+
+    list.forEach((keyword) => {
+        const li = document.createElement("li");
+        li.textContent = keyword;
+        autoCompleteArea.appendChild(li);
+    });
+}
